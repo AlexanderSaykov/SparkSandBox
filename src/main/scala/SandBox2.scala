2696d4d0d07477290f639f20092641eff93e03c6
AlexanderSaykov <alexsaykov@gmail.com>
2022-01-21 15:32:46 +0300

    Revert "3"
    This reverts commit e9095936b667fb4935765dad0e0601b496e2b179.

--- src/main/scala/SandBox2.scala
+++ src/main/scala/SandBox2.scala
@@ -16,7 +16,6 @@ object SandBox2 {
   def main(args: Array[String]): Unit = {
     1
     2
-    3
     val spark = SparkSession.builder.config("spark.driver.memory", "10G").appName("SandBox2").master("local").getOrCreate()
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
