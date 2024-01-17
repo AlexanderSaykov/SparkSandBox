a4e7e7c15fee494099f41026505f4b81e2695881
AlexanderSaykov <79256653+AlexanderSaykov@users.noreply.github.com>
2024-01-17 11:04:59 +0300

    Update CodeReview.scala

--- src/main/scala/CodeReview.scala
+++ src/main/scala/CodeReview.scala
@@ -1,39 +1,49 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-object CodeReview {
-  def main(args:Array[String]) ={
-    val spark = SparkSession.builder().master("local").getOrCreate()
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, max, col, when
 
-    import spark.implicits._
+# Create a SparkSession
+spark = SparkSession.builder.master("local").getOrCreate()
 
-    val df = spark.read.json("C/rnu_4_5_json") //очень большая таблица > 10 ТБ
+# Read a large JSON file (>10 TB)
+df = spark.read.json("C/rnu_4_5_json")
 
-    val dt_accountCount = df.groupBy("id_pk").agg(count("dt_account").as("dtCount")).filter(col("id_pk") =!= 117)
+# Group by 'id_pk' and count 'dt_account', exclude 'id_pk' 117
+dt_accountCount = df.groupBy("id_pk").agg(count("dt_account").alias("dtCount")).filter(col("id_pk") != 117)
 
-    val kt_accountCount = df.groupBy("id_pk").agg(count("kt_account").as("ktCount")).filter(col("id_pk") =!= 117)
+# Group by 'id_pk' and count 'kt_account', exclude 'id_pk' 117
+kt_accountCount = df.groupBy("id_pk").agg(count("kt_account").alias("ktCount")).filter(col("id_pk") != 117)
 
-    val dt_max = df.groupBy("id_pk").agg(max("dt_account").as("dtCountMax"))
+# Find max of 'dt_account' for each 'id_pk'
+dt_max = df.groupBy("id_pk").agg(max("dt_account").alias("dtCountMax"))
 
-    val kt_max = df.groupBy("id_pk").agg(max("kt_account").as("ktCountMax"))
+# Find max of 'kt_account' for each 'id_pk'
+kt_max = df.groupBy("id_pk").agg(max("kt_account").alias("ktCountMax"))
 
-    val resultDf = dt_accountCount
-      .join(kt_accountCount, Seq("id_pk"))
-      .join(dt_max, Seq("id_pk"))
-      .join(kt_max, Seq("id_pk"))
-      .filter(col("id_pk") =!= 7747)
+# Join the dataframes and exclude 'id_pk' 7747
+resultDf = dt_accountCount \
+    .join(kt_accountCount, ["id_pk"]) \
+    .join(dt_max, ["id_pk"]) \
+    .join(kt_max, ["id_pk"]) \
+    .filter(col("id_pk") != 7747)
 
-    val df2Dict = spark.read.json("C/dictionary") // маленькая таблица, меньше 10 мб
+# Read a small JSON file (smaller than 10 MB)
+df2Dict = spark.read.json("C/dictionary")
 
-    val ListFromDF2 = df2Dict.select(col("id_pk")).map(x => x.getLong(0).toString).collect().toList
+# Create a list from df2Dict 'id_pk'
+ListFromDF2 = [row["id_pk"] for row in df2Dict.select("id_pk").collect()]
 
-    val veryFinalResult = resultDf.filter(!col("id_pk").isin(ListFromDF2:_*))
+# Filter 'resultDf' to exclude rows present in 'ListFromDF2'
+veryFinalResult = resultDf.filter(~col("id_pk").isin(ListFromDF2))
 
-    veryFinalResult.show(10, false)
+# Show the first 10 rows
+veryFinalResult.show(10, truncate=False)
 
-    val dict2 = spark.read.json("C/dict2") // маленькая таблица, меньше 10 мб
+# Read another small JSON file (smaller than 10 MB)
+dict2 = spark.read.json("C/dict2")
 
-    veryFinalResult.join(dict2, Seq("id_pk"), "left").withColumn("izz_number", when(col("izz_number").isNull, "dssf111").otherwise(col("izz_number")))
+# Join 'veryFinalResult' with 'dict2' and handle nulls in 'izz_number'
+finalDf = veryFinalResult.join(dict2, ["id_pk"], "left") \
+    .withColumn("izz_number", when(col("izz_number").isNull(), "dssf111").otherwise(col("izz_number")))
 
-    veryFinalResult.write.csv("folder")
-  }
-}
+# Write the result to a CSV file
+finalDf.write.csv("folder")
